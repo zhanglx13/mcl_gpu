eee4bc5c68532521fd14b451f1aca58cae542d2b
Lixun <zhanglx@utexas.edu>
2021-06-29 21:34:21 -0500

    Make CircularArray a template class`

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,27 +1,6 @@
 #include "timer.h"
-#include <numeric>
 
-Utils::CircularArray::CircularArray(int size)
-{
-    arr.resize(size);
-    ind = 0;
-    num_els = 0;
-}
-
-void Utils::CircularArray::append(float val)
-{
-    if (num_els < (int)arr.size())
-        num_els ++;
-    arr[ind] = val;
-    ind = (ind + 1) % arr.size();
-}
-
-float Utils::CircularArray::mean()
-{
-    return std::accumulate(arr.begin(), arr.begin()+num_els, 0.0) / num_els;
-}
-
-Utils::Timer::Timer(int smoothing): arr(Utils::CircularArray(smoothing))
+Utils::Timer::Timer(int smoothing): arr(Utils::CircularArray<float>(smoothing))
 {
     last_time = Clock::now();
 }
--- src/timer.h
+++ src/timer.h
@@ -4,24 +4,48 @@
 #include <ros/ros.h>
 #include <vector>
 #include <chrono>
+#include <numeric>
 
 //using Clock = std::chrono::high_resolution_clock;
 using Clock = ros::Time;
 
 namespace Utils
 {
+    template <class T>
     class CircularArray
     {
     public:
         CircularArray(int size);
-        void append(float val);
-        float mean();
+        void append(T val);
+        T mean();
     protected:
-        std::vector<float> arr;
+        std::vector<T> arr;
         int num_els;
         int ind;
     };
 
+    template <class T>
+    CircularArray<T>::CircularArray(int size)
+    {
+        arr.resize(size);
+        ind = 0;
+        num_els = 0;
+    }
+
+    template <class T>
+    void CircularArray<T>::append(T val)
+    {
+        if (num_els < (int)arr.size())
+            num_els ++;
+        arr[ind] = val;
+        ind = (ind + 1) % arr.size();
+    }
+
+    template <class T>
+    T CircularArray<T>::mean()
+    {
+        return std::accumulate(arr.begin(), arr.begin()+num_els, 0.0) / num_els;
+    }
 
 
     class Timer
@@ -31,7 +55,7 @@ namespace Utils
         float tick();
         float fps();
     protected:
-        CircularArray arr;
+        CircularArray<float> arr;
         Clock last_time;
     };
 }
